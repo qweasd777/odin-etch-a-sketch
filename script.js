8f981f5e475eb959e1eb790e2980512b849af256
Masq <12148021+qweasd777@users.noreply.github.com>
2024-02-07 15:27:33 +0800

    Add skeletal framework & files (index.html,css,js)

---
+++ script.js
@@ -0,0 +1,16 @@
+// const btn = document.querySelector('#test');
+
+// function test() {
+//     btn.innerHTML = "";
+//     btn.style.display = "none";
+// }
+
+// // BUTTONS
+// btn.addEventListener('click', () => {  
+// });
+const container = document.querySelector('#container');
+const grid = document.createElement('div');
+
+grid.id = "grid";
+
+container.appendChild(grid);
