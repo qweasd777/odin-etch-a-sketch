29d393d9fc7d2e4141cceececc26edeb4629714f
Masq <12148021+qweasd777@users.noreply.github.com>
2024-02-08 17:20:09 +0800

    Improve sketch feature & add functional clear btn
    Improvements:
    - user can now hold down and release mouse to start and stop sketching respectively

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-// const btn = document.querySelector('#test');
-
-// function test() {
-//     btn.innerHTML = "";
-//     btn.style.display = "none";
-// }
-
-// // BUTTONS
-// btn.addEventListener('click', () => {  
-// });
-
-const GRID_SIZE = 16;
+const GRID_SIZE_ORIGIN = 16;
 const container = document.querySelector('.container'); // grid wrap
 const grid = document.createElement('div');             // fullgrid 
 const grid_css = `  display: flex; 
@@ -18,33 +7,72 @@ const grid_css = `  display: flex;
 grid.classList.add('grid');
 grid.style.cssText = grid_css;
 
-for(let i = 0; i < GRID_SIZE; ++i)
-{
-    const row = document.createElement('div');
-    row.classList.add('row');
-    row.style.display = "flex";
-    row.style.flex = "1";
+// ---------- BUTTONS ----------
+const btn_clear = document.querySelector('#setClear');
+// ---------- BUTTONS ----------
 
-    for(let j = 0; j < GRID_SIZE; ++j)
+let gridSize = GRID_SIZE_ORIGIN;
+let brushActive = false;
+
+function gridInit() {
+    for(let i = 0; i < gridSize; ++i)
     {
-        const col = document.createElement('div');
-        col.classList.add('col');
-        col.style.flex = "1";
-        col.style.backgroundColor = "#ccc";
-        col.style.minHeight = "10px";
-        col.style.minWidth = "10px";
-        row.appendChild(col);
+        const row = document.createElement('div');
+        row.classList.add('row');
+        row.style.display = "flex";
+        row.style.flex = "1";
+
+        for(let j = 0; j < gridSize; ++j)
+        {
+            const col = document.createElement('div');
+            col.classList.add('col');
+            col.style.flex = "1";
+            col.style.backgroundColor = "#fff";
+            col.style.minHeight = "10px";
+            col.style.minWidth = "10px";
+            col.draggable = false;
+            row.appendChild(col);
+        }
+        grid.appendChild(row);
     }
-    grid.appendChild(row);
+    container.appendChild(grid);
 }
-//const col2 = col.cloneNode(true);
 
-container.appendChild(grid);
+const colourGridBox = (e) => {
+    let box = e.target;
 
-// Colour onClick test
-grid.addEventListener('click', (e) => {
-    // check if grid clicked is uncoloured
-    if (e.target.matches('.col')) {
-        e.target.style.background = "black";
-    }
+    // CURRENT COLOUR CHOICE
+    box.style.background = "#333";
+};
+
+gridInit();
+
+// ---------- MOUSE EVENTS ---------- 
+grid.addEventListener('mousedown', (e) => {
+    e.target.style.background = "#333";
+    // Add listener to gridContainer on mousedown
+    grid.addEventListener('mouseover', colourGridBox);
 });
+window.addEventListener('mouseup', () => {
+    // Remove listener from gridContainer on mousedown
+    grid.removeEventListener('mouseover', colourGridBox);
+});
+
+// Drag & drop prevention on gridContainer
+grid.addEventListener('dragstart', (e) => {
+    e.preventDefault();
+});
+grid.addEventListener('drop', (e) => {
+    e.preventDefault();
+});
+// ---------- MOUSE EVENTS ---------- 
+// ---------- BUTTON EVENTS ---------- 
+btn_clear.addEventListener('click', () => {      
+    const boxes = document.querySelectorAll('.col');
+
+    // Reset BG colour to white
+    boxes.forEach(box => {
+        box.style.backgroundColor = "#fff";
+    });
+});
+// ---------- BUTTON EVENTS ---------- 
