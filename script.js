4b5778f93a4a4d8ed5dcd598ecdf98dbb4e1e47e
Masq <12148021+qweasd777@users.noreply.github.com>
2024-02-09 18:08:57 +0800

    Add resizable grid size, brushes & update css
    Resizable grid size:
    - user can now interact with slider to adjust grid size
    
    Brushes:
    - user can now select among normal, RGB or eraser brushes
    
    CSS updates:
    - fix spacing of container and UI
    - slider

--- script.js
+++ script.js
@@ -6,15 +6,35 @@ const grid_css = `  display: flex;
                     flex: 1;`;
 grid.classList.add('grid');
 grid.style.cssText = grid_css;
+const gridSizeText = document.querySelector("#gridsizeText");
+gridSizeText.innerHTML = GRID_SIZE_ORIGIN;
 
-// ---------- BUTTONS ----------
-const btn_clear = document.querySelector('#setClear');
-// ---------- BUTTONS ----------
+const Brush = {
+	normal: "normal",
+	eraser: "eraser",
+	shader: "shader",
+	rgb: "rgb"
+}
+
+const BrushColour = {
+	normal: "#333",
+	eraser: "#fff"
+}
 
 let gridSize = GRID_SIZE_ORIGIN;
-let brushActive = false;
+let brushType = Brush.normal;
+
+// ---------- BUTTONS ----------
+const btnAll_brushes = document.querySelectorAll('.brushType');
+const btn_clear = document.querySelector('#setClear');
+const slider = document.querySelector(".slider");
+slider.value = GRID_SIZE_ORIGIN;    // slider init value
+// ---------- BUTTONS ----------
 
 function gridInit() {
+    // Reset grid
+    grid.innerHTML = "";
+
     for(let i = 0; i < gridSize; ++i)
     {
         const row = document.createElement('div');
@@ -27,9 +47,9 @@ function gridInit() {
             const col = document.createElement('div');
             col.classList.add('col');
             col.style.flex = "1";
-            col.style.backgroundColor = "#fff";
-            col.style.minHeight = "10px";
-            col.style.minWidth = "10px";
+            col.style.backgroundColor = BrushColour.eraser;
+            //col.style.minHeight = "10px";
+            //col.style.minWidth = "10px";
             col.draggable = false;
             row.appendChild(col);
         }
@@ -38,18 +58,47 @@ function gridInit() {
     container.appendChild(grid);
 }
 
-const colourGridBox = (e) => {
-    let box = e.target;
+function randomHSL() {
+    // function to return only bright rgb colors
+    // by kigiri (https://stackoverflow.com/questions/1484506/random-color-generator)
+    return 'hsla(' + (Math.random() * 360) + ', 100%, 50%, 1)';
+}
 
-    // CURRENT COLOUR CHOICE
-    box.style.background = "#333";
-};
+function colourGridBox(event) {
+    let box = event.target;
+
+    // Colour box according to current brushType
+    switch(brushType)
+    {
+        case Brush.normal:
+            box.style.backgroundColor = BrushColour.normal;
+            break;
+
+        case Brush.eraser:
+            box.style.backgroundColor = BrushColour.eraser;
+            break;
+
+        case Brush.shader:
+            //const title = document.querySelector(".title");
+            //title.innerHTML = box.style.getAttribute();
+            //box.style.filter = "brightness(0.5)";
+            
+            break;
+
+        case Brush.rgb:
+            box.style.backgroundColor = randomHSL();
+            break;
+
+        default:
+            alert("undefined brushType");
+    }
+}
 
 gridInit();
 
 // ---------- MOUSE EVENTS ---------- 
 grid.addEventListener('mousedown', (e) => {
-    e.target.style.background = "#333";
+    colourGridBox(e);
     // Add listener to gridContainer on mousedown
     grid.addEventListener('mouseover', colourGridBox);
 });
@@ -67,12 +116,25 @@ grid.addEventListener('drop', (e) => {
 });
 // ---------- MOUSE EVENTS ---------- 
 // ---------- BUTTON EVENTS ---------- 
-btn_clear.addEventListener('click', () => {      
-    const boxes = document.querySelectorAll('.col');
-
-    // Reset BG colour to white
-    boxes.forEach(box => {
-        box.style.backgroundColor = "#fff";
+btnAll_brushes.forEach((btn_brush) => {
+    // for each brush btn we add a 'click' listener
+    btn_brush.addEventListener('click', (e) => {
+        brushType = btn_brush.id;
     });
 });
+
+btn_clear.addEventListener('click', () => {      
+    gridInit();
+});
+
+// SLIDER UPDATE
+slider.onchange = function() {
+    gridSize = this.value;
+    
+    gridInit();     
+}
+slider.oninput = function() {
+    // Update gridSize info display
+    gridSizeText.innerHTML = this.value;
+}
 // ---------- BUTTON EVENTS ---------- 
